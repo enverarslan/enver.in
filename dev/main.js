3239533cc7c8ff62cc447699b31043b843f33823
Enver Arslan <enver@programmer.net>
2016-10-25 21:18:54 +0300

    gulp + media query + package

---
+++ dev/main.js
@@ -0,0 +1,65 @@
+(function(window){
+
+    var wW = window.innerWidth - 10;
+    var wH = window.innerHeight - 10;
+
+
+    window.onresize = function () {
+        wW = window.innerWidth - 10;
+        wH = window.innerHeight - 10;
+        play(wW, wH);
+    };
+
+
+    for (var i = 0; i < 100; i++) {
+        var s = document.createElement('i');
+        s.className = "star";
+        document.body.querySelector('.space').appendChild(s);
+    }
+
+    var stars = [].slice.call(document.getElementsByClassName('star'));
+
+
+    function play(width, height) {
+        shuffle(stars).map(function (star, index) {
+            var left = random(width);
+            var top = random(height);
+
+            star.style["width"] = "1px";
+            star.style["height"] = "1px";
+
+            if ((index % 3 == 0)) {
+                star.style["width"] = "3px";
+                star.style["height"] = "3px";
+            }
+            star.style["top"] = top + "px";
+            star.style["left"] = left + "px";
+        });
+    }
+
+
+    function random(size) {
+        var rand = Math.round(Math.random() * size);
+        return (rand >= size) ? random(size) : rand;
+    }
+
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+
+    play(wW, wH);
+    setInterval(function () {
+        play(wW, wH);
+    }, 12000);
+})(window);
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    prefixer = require('gulp-autoprefixer'),
+    minifyCss = require('gulp-minify-css'),
+    plumber = require('gulp-plumber'),
+    fs = require('fs');
+
+
+gulp.task('js', function () {
+    return gulp.src('./dev/*.js')
+        .pipe(plumber())
+        .pipe(uglify({compress: true}))
+        .pipe(gulp.dest('./assets'));
+});
+
+gulp.task('css', function () {
+    return gulp
+        .src('dev/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(prefixer({
+            browsers: ["last 1 version", "> 1%", "ie 8", "ie 7"]
+        }))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./assets'));
+});
+
+gulp.task('clean', function () {
+    var dir = './assets/';
+    fs.readdir(dir, function (err, files) {
+        if (files.length) {
+            files.forEach(function (file, index) {
+                fs.unlink(dir + file);
+            });
+        }
+    });
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch(['./dev/*.js'], ['js']);
+    gulp.watch(['./dev/*.scss'], ['css']);
+});
+
+gulp.task('default', ['clean', 'css', 'js', 'watch']);
